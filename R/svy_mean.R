292542a605881035111c5f54b18a08a0b4109051
Sebastian Nickel <senickel@yahoo.de>
2018-06-25 19:54:56 +0200

    Update svy_mean.R

--- R/svy_mean.R
+++ R/svy_mean.R
@@ -12,17 +12,42 @@ svy_mean<-function(data,
   if (is.na(svyset)) stop("No svyset specified.")
 
   # check if data is a data.frame
+  if (!any(grepl("data.frame",is(data))))
+    stop("'data' is not of class data.frame")
+
+  # check if all numeric
+  if (!all(apply(data,is.numeric)))
+    stop("All columns need to be numeric.")
   if (!any(grepl("data.frame",is(data)))) stop("data is not of class data.frame")
 
   temp_folder<-reduce_data_and_temp_folder(data,mean_var,over,subpop,svyset)
 
   # create beginning of do file
+  do_file_step1<-
+    c(
+      paste0("clear all\nset more off\nglobal path \"",getwd(),"/",
+             temp_folder,"/\""),
+      "import delimited \"${path}data.csv\", delimiter(comma)",
+      " bindquote(strict) varnames(1) case(preserve) clear",
+      svyset)
   do_file_step1<-c(paste0("clear all\nset more off\nglobal path \"",getwd(),"/",temp_folder,"/\""),
                    "import delimited \"${path}data.csv\", delimiter(comma) bindquote(strict) varnames(1) case(preserve) clear",
                    svyset)
   if (!is.na(recodes)) do_file_step1<-c(do_file_step1,recodes)
   do_file<-c(do_file_step1,
-             "program def write_mean_table\n	if  (missing(\"`3'\")) {\n		capture svy: mean `1', over(`2')\n	}\n	else {\n		capture svy, subpop(if `3'): mean `1', over(`2')\n	}\nif !_rc {\n	mat A = r(table)\n	mat2txt, matrix(A) saving(\"${path}/`1'_over_`2'.txt\") replace\n} \nelse {\nlocal errorm = _rc\nfile open out1 using `\"${path}/`1'_over_`2'.txt\"', w replace\nfile write out1 \"errorcode: `errorm'\"\nfile close out1\n}\nend\n")
+             "program def write_mean_table\n	if  (missing(\"`3'\")) {
+capture svy: mean `1', over(`2')\n	}\n	else {
+             capture svy, subpop(if `3'): mean `1', over(`2')
+             }
+             if !_rc {
+             mat A = r(table)
+             mat2txt, matrix(A) saving(\"${path}/`1'_over_`2'.txt\") replace
+             }
+             else {\nlocal errorm = _rc
+             file open out1 using `\"${path}/`1'_over_`2'.txt\"', w replace
+             file write out1 \"errorcode: `errorm'\"
+             file close out1
+             }\nend\n")
   if (is.na(subpop)) {
     do_file<-c(do_file,paste("write_mean_table",mean_var,over))
   } else {
@@ -38,22 +63,27 @@ svy_mean<-function(data,
   close(file1)
 
   # execute do file
-  execute_do_file(path_to_do_file = paste0(getwd(),"/",temp_folder,"/do_file.do"))
+  execute_do_file(path_to_do_file = paste0(getwd(),"/",temp_folder,
+                                           "/do_file.do"))
 
   # read in results
   if (!file.exists(paste0(temp_folder,"/",mean_var,"_over_",over,".txt"))) {
     # clean up
     unlink(paste0(getwd(),"/",temp_folder),force = TRUE,recursive = TRUE)
 
+    stop("Execution failed. No Errorcode in Stata generated.
+         Was the do file executed in Stata?")
     stop("Execution failed. No Errorcode in Stata generated. Was the do file executed in Stata?")
   }
 
-  check_error<-readLines(paste0(temp_folder,"/",mean_var,"_over_",over,".txt"),warn=FALSE)
+  check_error<-readLines(
+    paste0(temp_folder,"/",mean_var,"_over_",over,".txt"),warn=FALSE)
   if (check_error %>% length==1&grepl("errorcode: ",check_error[1])) {
     # clean up
     unlink(paste0(getwd(),"/",temp_folder),force = TRUE,recursive = TRUE)
 
-    stop(paste("Execution failed. Stata returns errorcode",check_error[1] %>% gsub("errorcode: ","",.)))
+    stop(paste("Execution failed. Stata returns errorcode",check_error[1] %>%
+                 gsub("errorcode: ","",.)))
 
   }
   tab<-read.table(paste0(temp_folder,"/",mean_var,"_over_",over,".txt"))
